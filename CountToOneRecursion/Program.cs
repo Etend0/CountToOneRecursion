0afcf93aac82304cb88e7e359a2d398752543b2b
Etendo <etendo123@gmail.com>
2026-03-16 02:54:43 -0600

    Implemented count to one challenges.

--- CountToOneRecursion/Program.cs
+++ CountToOneRecursion/Program.cs
@@ -11,25 +11,26 @@
 //-------------------------------------------------------
 
 // Declare and initialize
-int choice = 0, result = 0;
+int choice = 0, result = 0, counter = 0;
 string input = "";
 
 // Prompt the user for a number
-Console.Write("Enter a positive number: ");
+Console.Write("Enter a number (positive or negative): ");
 // Get the users input
 input = Console.ReadLine();
 // See if the user entered a valid input
-while (!int.TryParse(input, out choice) && choice > 0)
+while (!int.TryParse(input, out choice))
 {
-    Console.Write("Invalid number");
+    Console.Write("Invalid number. ");
     // Re-Prompt the user for a number
-    Console.Write("Enter a positive number: ");
+    Console.Write("Enter a number (positive or negative): ");
     // Get the users input
     input = Console.ReadLine();
 }
 // Call the CountToOne function
-result = Utility.CountToOne(choice);
+result = Utility.CountToOne(choice, ref counter);
 Console.WriteLine($"The end number is {result}");
+Console.WriteLine($"It took {counter} times to get down to 1.");
 
 //-------------------------------------------------------
 // End of the Main Method
@@ -45,29 +46,86 @@ Console.WriteLine($"The end number is {result}");
 /// <returns></returns>
 static class Utility
 {
-    internal static int CountToOne(int num)
+    internal static int CountToOne(int num, ref int count)
     {
         // Print out the current number
         Console.WriteLine($"The current number is {num}");
         // Check if the number is 1
-        if (num <= 1)
+        if (num == 1)
         {
             return 1;
         }
-        else
+        // Check if the number is a negative or zero
+        else if (num < 1)
         {
-            // Check if the number is even
-            if ((num % 2) == 0)
+            // Check if the number is 0
+            if (num == 0)
             {
-                Console.WriteLine("The number is even. Divide by 2");
-                // Divide the number by 2 and call the function
-                return CountToOne(num / 2);
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is 0. Add 1.");
+                // Add 1 to the number and call the function
+                return CountToOne(num + 1, ref count);
+            }
+            // Check if the number is divisible by 7
+            else if ((num % 7) == 0)
+            {
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is negative and divisible by 7. Multiply by 3 and add 1.");
+                // Multiply the number by 3 and add 1
+                num = num * 3;
+                num++;
+                // Call the function
+                return CountToOne(num, ref count);
+            }
+            // Check if the number is even
+            else if ((num % 2) == 0)
+            {
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is negative and even. Divide by 2");
+                // ievide the number by 2 and call the function
+                return CountToOne(num / 2, ref count);
             }
             else
             {
-                Console.WriteLine("The number is odd. Add 1");
-                // If the number is odd, subtract 1 from it
-                return CountToOne(num + 1);
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is negative and odd. Add 1");
+                // Add 1 to the number and call the function
+                return CountToOne(num + 1, ref count);
+            }
+        }
+        // Otherwise it's positive
+        else
+        {
+            if ((num % 7) == 0)
+            {
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is divisible by 7. Multiply by 3 and subtract 1.");
+                // Multiply the number by 3 and subtract 1
+                num = num * 3;
+                num--;
+                // Call the function
+                return CountToOne(num, ref count);
+            }
+            else if ((num % 2) == 0)
+            {
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is even. Divide by 2");
+                // Divide the number by 2 and call the function
+                return CountToOne(num / 2, ref count);
+            }
+            else
+            {
+                // Incremenet the counter
+                count++;
+                Console.WriteLine("The number is odd. Subtract 1");
+                // Subtract 1 from the number and call the function
+                return CountToOne(num - 1, ref count);
             }
         }
     }
